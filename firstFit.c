bcd28f7311fe60ca45fbfb013fc8f40bc2694b77
hikkywannafly <ggraygon@gmail.com>
2023-09-24 20:20:14 +0700

    feat: add frist fit explain

--- firstFit.c
+++ firstFit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_MEMORY_SIZE 1000
+#define MAX_MEMORY_SIZE 1000  // tổng size của memory
 
 typedef struct {
     int start_address;
@@ -10,8 +10,10 @@ typedef struct {
     int is_allocated;
 } MemoryBlock;
 
+// function để khởi tạo các memory block
 void initialize_memory_blocks(MemoryBlock memory_blocks[], int num_blocks) {
     int i;
+    // khởi tạo các memory block
     for (i = 0; i < num_blocks; i++) {
         memory_blocks[i].start_address = i * 100;
         memory_blocks[i].end_address = (i + 1) * 100 - 1;
@@ -19,7 +21,7 @@ void initialize_memory_blocks(MemoryBlock memory_blocks[], int num_blocks) {
         memory_blocks[i].is_allocated = 0;
     }
 }
-
+// function để in các memory block
 void print_memory_blocks(MemoryBlock memory_blocks[], int num_blocks) {
     int i;
     for (i = 0; i < num_blocks; i++) {
@@ -28,17 +30,19 @@ void print_memory_blocks(MemoryBlock memory_blocks[], int num_blocks) {
     }
 }
 
+// function để cấp phát bộ nhớ
 int allocate_memory_first_fit(MemoryBlock memory_blocks[], int num_blocks, int size) {
     int i;
     for (i = 0; i < num_blocks; i++) {
-        if (!memory_blocks[i].is_allocated && memory_blocks[i].size >= size) {
-            memory_blocks[i].is_allocated = 1;
-            return memory_blocks[i].start_address;
+        if (!memory_blocks[i].is_allocated && memory_blocks[i].size >= size) { // nếu block chưa được cấp phát và có đủ size để cấp phát
+            memory_blocks[i].is_allocated = 1; // cấp phát bộ nhớ
+            return memory_blocks[i].start_address;  // return địa chỉ bắt đầu của block được cấp phát
         }
     }
-    return -1;
+    return -1;  // return -1 nếu không tìm thấy vị trí cấp phát bộ nhớ
 }
 
+// function để giải phóng bộ nhớ
 void free_memory(MemoryBlock memory_blocks[], int num_blocks, int start_address) {
     int i;
     for (i = 0; i < num_blocks; i++) {
